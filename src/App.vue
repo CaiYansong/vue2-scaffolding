f8bea3c8ca587864191525573f328c18ccf7b193
caiyansong <caiyansong@enn.cn>
2021-07-22 19:22:40 +0800

    feat: test local img

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,30 @@
     </div>
     -{{ JSON.stringify(state) }}
     <router-view />
+    ./
+    <img style="width: 100px;" :src="require('./assets/test-729kb.jpeg')" alt="">
+    @/
+    <img style="width: 100px;" :src="require('@/assets/test-729kb.jpeg')" alt="">
+    import:
+    ./
+    <img style="width: 100px;" :src="testImg" alt="">
+    @/
+    <img style="width: 100px;" :src="testImgA" alt="">
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import testImg from './assets/test-729kb.jpeg';
+import testImgA from '@/assets/test-729kb.jpeg';
 
 export default {
+  data() {
+    return {
+      testImg,
+      testImgA,
+    }
+  },
   computed: {
     // 通过 global 获取 全局 state 的信息
     ...mapState('global', {
