79d73ced4d401b04dc5f1b2438fc943f2e10fb54
caiyansong <caiyansong@enn.cn>
2021-07-22 14:14:35 +0800

    fix: less problem

--- src/public-path.js
+++ src/public-path.js
@@ -1,3 +1,14 @@
+// 解决引入 less 导致页面空白的问题
+if (!window.less) {
+  window.less = {
+    async: true,
+  };
+} else {
+  window.less.async = true;
+}
+
+const { name } = require('../package.json');
+
 (function () {
   // eslint-disable-next-line no-underscore-dangle
   if (window.__POWERED_BY_QIANKUN__) {
@@ -7,13 +18,6 @@
       return;
     }
     // eslint-disable-next-line
-    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
-  }
-
-  // 解决引入 less 导致页面空白的问题
-  if (!window.less) {
-    window.less = {
-      async: true,
-    };
+    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ + 'micro/' + name;
   }
 }());
