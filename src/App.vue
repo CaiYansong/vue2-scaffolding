43444497bb9691b5505b1c5961ecd93be01e7a23
caiyansong <caiyansong@enn.cn>
2021-07-22 19:21:57 +0800

    feat: test local img

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,29 @@
       </router-link>
     </div>
     <router-view />
+    ./
+    <img style="width: 100px;" :src="require('./assets/test-729kb.jpeg')" alt="">
+    @/
+    <img style="width: 100px;" :src="require('@/assets/test-729kb.jpeg')" alt="">
+    import:
+    ./
+    <img style="width: 100px;" :src="testImg" alt="">
+    @/
+    <img style="width: 100px;" :src="testImgA" alt="">
   </div>
 </template>
 
 <script>
-export default {
+import testImg from './assets/test-729kb.jpeg';
+import testImgA from '@/assets/test-729kb.jpeg';
 
+export default {
+  data() {
+    return {
+      testImg,
+      testImgA,
+    }
+  },
 };
 </script>
 
