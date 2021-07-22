0c9dde62a99b161671c69795c89891a0c75ff469
caiyansong <caiyansong@enn.cn>
2021-07-22 13:33:44 +0800

    fix: fix less problem

--- src/public-path.js
+++ src/public-path.js
@@ -1,6 +1,19 @@
-/* eslint-disable no-underscore-dangle */
-// qiankun 环境下处理资源
-if (window.__POWERED_BY_QIANKUN__) {
-  // eslint-disable-next-line camelcase
-  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
-}
+(function () {
+  // eslint-disable-next-line no-underscore-dangle
+  if (window.__POWERED_BY_QIANKUN__) {
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line
+      __webpack_public_path__ = `//localhost:${process.env.VUE_APP_PORT}${process.env.BASE_URL}`
+      return;
+    }
+    // eslint-disable-next-line
+    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
+  }
+
+  // 解决引入 less 导致页面空白的问题
+  if (!window.less) {
+    window.less = {
+      async: true,
+    };
+  }
+}());
